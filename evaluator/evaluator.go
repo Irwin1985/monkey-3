7f2c4db80d6642b3c82c1b28ab4e06983449bf37
gcoka <y.okamoto@gemcook.com>
2018-08-26 01:53:12 +0900

    [add] null and bool

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
 	"github.com/gcoka/monkey/object"
 )
 
+// pre-defined values.
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // Eval evaluates AST.
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -14,6 +21,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBooleanObject(node.Value)
 	}
 
 	return nil
@@ -27,4 +36,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
